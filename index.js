99e754fa9b539b4ae69dd65c8be521be6d830a23
Connor Henn <connor.henn@yahoo.com>
2022-07-27 21:05:22 -0700

    commit tree 644e6310738734d160c257288d82c63fa5ec640b

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Add your code here
+function submitData(username, userEmail){
+fetch(`http://localhost:3000/users`, {
+    method: 'POST',
+    headers:{
+    'Content-Type': 'application/json',
+    'Accept': 'application/json',
+    },
+    body: JSON.stringify({
+      name: username,
+      email: userEmail,
+    })
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (object) {
+      console.log(object);
+    })
+  });
+}
